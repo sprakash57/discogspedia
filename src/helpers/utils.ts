e951f5afb0c4467f6aadb5079f14414827261fb7
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-08-30 01:15:43 +0530

    fix: Ellipsis will be applicable in certain situations

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,6 +1,7 @@
 export const clsx = (...args: any[]) => args.filter(arg => !!arg).join(" ");
 
-export const ellipsisText = (text: string, letters: number = 40) => {
+export const ellipsisText = (text: string, letters: number = 60) => {
     if (!text) return "--";
+    if (text.length <= 60) return text;
     return text.slice(0, letters).concat("...");
 }
